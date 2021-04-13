0adb62307adce1d4ca6ff3ebe26acbf23e6527e8
ashwineec <ashwineec.official@gmail.com>
2021-04-13 13:35:55 +0530

    Test

--- FoodOrderingApp-api/src/test/java/com/upgrad/FoodOrderingApp/api/controller/AddressControllerTest.java
+++ FoodOrderingApp-api/src/test/java/com/upgrad/FoodOrderingApp/api/controller/AddressControllerTest.java
@@ -38,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AddressControllerTest {
+public class AddressControllerTest1 {
 
     @Autowired
     private MockMvc mockMvc;
